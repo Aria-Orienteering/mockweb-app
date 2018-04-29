979bf1a94558f5a552c64ad5dbf2e17d600e52ca
lxdnz254 <lxdnz34@gmail.com>
2018-04-29 22:39:54 +1200

    Live Selected user updates of location, just as text in a box, still figuring MapContainer marking

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React, { Component} from 'react' ;
 import PubSub from  'pubsub-js'
+import {getUsersThunk, watchUserChangedEvent} from "../store/Store";
+import {connect} from "react-redux";
 
 class Hello extends Component {
     constructor() {
         super();
         this.state = {
-            selection: 'none'
+            selection: 'none',
+
         }
     }
 
@@ -17,14 +20,42 @@ class Hello extends Component {
         });
     }
 
+
+
     componentWillUnmount() {
         // React removed me from the DOM, I have to unsubscribe from the system using my token
         PubSub.unsubscribe(this.token);
     }
 
+
+    box(i) {
+        let user = this.props.users[i];
+        return <div>
+            <p>You have selected the user: {user.firstName}</p>
+            <p>Longitude: {user.lon}</p>
+            <p>Latitude: {user.lat}</p>
+        </div>
+    }
+
     render() {
-        return <div>You have selected the user: {this.state.selection.firstName}</div>
+        let i = this.props.users.findIndex(i => i.firstName === this.state.selection.firstName)
+        if (i > -1) {
+            return this.box(i);
+        } else {
+            return <div>
+                <p>You have selected no-one</p>
+            </div>
+        }
     }
 }
 
-export default Hello;
+const mapState = state => ({
+    users: state
+});
+const mapDispatch = dispatch => {
+    dispatch(getUsersThunk());
+    watchUserChangedEvent(dispatch);
+    return {
+    }
+};
+export default connect(mapState, mapDispatch)(Hello);
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import  GoogleMapReact from 'google-map-react'
 import '../css/MapContainer.css'
+import PubSub from "pubsub-js";
+import {getUsersThunk, watchUserChangedEvent} from "../store/Store";
+import {connect} from "react-redux";
 
 // const AnyReactComponent = ({ text }) => <div>{ text }</div>;
-const myLatLng = {lat: -38.560926, lng: 174.983468};
+let myLatLng = {lat: -38.560926, lng: 174.983468};
 
-export default class MapContainer extends Component {
+class MapContainer extends Component {
     static defaultProps = {
         options: {mapTypeControl: true,
             styles: [{ stylers: [{ 'saturation': -100 }, { 'gamma': 0.8 }, { 'lightness': 4 }, { 'visibility': 'on' }] }]
         }
     };
+    constructor() {
+        super();
+        this.state = {
+            center: [-38.560926, 174.983468],
+            zoom: 14,
+            selection: '',
+        };
+    }
 
     renderMarkers(map, maps) {
         return new maps.Marker({
@@ -18,13 +29,36 @@ export default class MapContainer extends Component {
             map,
             title: 'Aria Village'
         });
-
     }
 
-    state = {
-        center: [-38.560926, 174.983468],
-        zoom: 14,
-    };
+    renderUser(map, maps) {
+        console.log('In Render User');
+        let i = this.props.users.findIndex(i => i.firstName === this.state.selection.firstName)
+        if (i > -1) {
+            let user = this.props.users[i];
+            let userLatLng = {lat: user.lat, lng: user.lon};
+            map.add(maps.Marker({
+                position: userLatLng,
+                map,
+                title: user.firstName
+            }));
+        } else {
+            return null
+        }
+    }
+
+    componentWillMount() {
+        // React will mount me, I can subscribe to the topic 'products'
+        // `.subscribe()` returns a token used to unsubscribe
+        this.token = PubSub.subscribe('user', (topic, user) => {
+            this.setState({selection: user});
+        });
+    }
+
+    componentWillUnmount() {
+        // React removed me from the DOM, I have to unsubscribe from the system using my token
+        PubSub.unsubscribe(this.token);
+    }
 
     _onChange = ({center, zoom}) => {
         this.setState({
@@ -33,7 +67,10 @@ export default class MapContainer extends Component {
         });
     };
 
+
     render() {
+        const markers = ({map, maps}) => this.renderMarkers(map, maps);
+        let users = ({map, maps}) => this.renderUser(map, maps);
         return (
             <div className='google-map'>
                 <GoogleMapReact
@@ -42,14 +79,22 @@ export default class MapContainer extends Component {
                     center={this.state.center}
                     zoom={this.state.zoom}
                     options = {this.props.options}
-                    onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
+                    onGoogleApiLoaded={markers}
                     >
-
+                    {users}
                 </GoogleMapReact>
             </div>
         )
     }
 }
+const mapState = state => ({
+    users: state
+});
+const mapDispatch = dispatch => {
+    dispatch(getUsersThunk());
+    watchUserChangedEvent(dispatch);
+    return {
+    }
+};
 
-
-
+export default connect(mapState, mapDispatch)(MapContainer)
